7c00500e295e49047fc17c30245d8c91d8022b35
Jasper den Ouden <o.jasper@gmail.com>
2013-10-06 01:58:24 +0200

    Okey, it seems to transfer files okey, but the publicly and privately shared info reach "the same spot" at the end.. it should be clear who it is okey to share with

--- test/test.sh
+++ test/test.sh
@@ -24,7 +24,16 @@ gpgshare send messages/hello_world hello_world.pubshare
 echo 'Mr.A says hello to Mr.B' > $DIR/hello_mr_b
 gpgshare send messages/hello_mr_b CE7D38D3 #thats mr.B
 
-#mv `gpgshare selfdir`/to_send/todo_alias/*.privshare \
-#`gpgshare selfdir`/to_send/todo_alias/*.privshare
+#Move into Bs directory
+mkdir -p Mr.B/store/raw_received/
+mv `gpgshare selfdir`/*.privshare \
+   `gpgshare selfdir`/*.pubshare Mr.B/store/raw_received/
+
+#Change identity
+export STORE=`pwd`/Mr.B/store
+export GNUPGHOME=`pwd`/Mr.B/dotgnupg/
+
+gpgshare receive Mr.B/store/raw_received/*.pubshare
+gpgshare receive Mr.B/store/raw_received/*.privshare
 
 #TODO Mr. B receiving both (check it is the same.
