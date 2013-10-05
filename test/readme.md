be111047a7aae99a32616741aab77ab8eeb5484c
Jasper den Ouden <o.jasper@gmail.com>
2013-10-06 01:38:08 +0200

    More testing and first bit of tests runs(now to receive it)

--- test/readme.md
+++ test/readme.md
@@ -1,7 +1,6 @@
 ### TODO
 Script to: 
 
-* Make some unencrypted test private keys in a local directory.
-  (with warning that they're invalid in the wild, also make them short)
 * Demonstrate how to do it.
+
 * Randomly send/fetch stuff and checking.
--- test/test.sh
+++ test/test.sh
@@ -9,10 +9,10 @@ gpgshare()
     sh ../gpgshare $@
 }
 
-export STORE=Mr.A/store
-export GNUPGHOME=Mr.A/dotgnupg
+export STORE=`pwd`/Mr.A/store
+export GNUPGHOME=`pwd`/Mr.A/dotgnupg/
 
-#TODO A,B know each other.
+#TODO A,B know each other. See Makefile.
 
 #Composing public messages(well, at least, not encrypted by gnupg)
 DIR=`gpgshare selfdir`/messages
@@ -20,11 +20,11 @@ mkdir -p $DIR
 echo 'Mr.A says hello to the world' > $DIR/hello_world
 gpgshare send messages/hello_world hello_world.pubshare
 
-#TODO compose for mr B
+#Compose for mr B 
 echo 'Mr.A says hello to Mr.B' > $DIR/hello_mr_b
+gpgshare send messages/hello_mr_b CE7D38D3 #thats mr.B
 
+#mv `gpgshare selfdir`/to_send/todo_alias/*.privshare \
+#`gpgshare selfdir`/to_send/todo_alias/*.privshare
 
 #TODO Mr. B receiving both (check it is the same.
-
-
-
